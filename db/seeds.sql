5d618d5c71ecc454ac8c1b39c85597b7fbe4abee
Dariell Vasquez <dariell@Dariells-Air.fios-router.home>
2017-11-27 21:34:21 -0500

    Wasn't in the correct directory on initial file setup commit, added DB

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Club BLT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quater Pounder', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
